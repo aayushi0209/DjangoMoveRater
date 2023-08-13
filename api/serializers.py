376acf51fb6c4126fbe987e4d5811a8a6400c934
AAYUSHIGUPTA\hp <aayushigupta0209@gmail.com>
2023-08-13 12:38:39 +0530

    authentication added

--- api/serializers.py
+++ api/serializers.py
@@ -6,7 +6,7 @@ from api.models import Movie, Rating
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ('id', 'title', 'description')
+        fields = ('id', 'title', 'description', 'no_of_rating','avg_rating')
 
 
 class RatingSerializer(serializers.ModelSerializer):
